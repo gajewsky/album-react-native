4532b4c212fd8d4b4a7d85a6b1fd2b60266c416e
gajewsky <marcin.gajewski@rebased.pl>
2017-01-26 13:11:49 +0100

    Header component

--- index.android.js
+++ index.android.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
 
 const App = () => (
-  <Text> Hello, World! </Text>
+  <Header text={'Albums'} />
 );
 
-AppRegistry.registerComponent('album', () => App)
+AppRegistry.registerComponent('album', () => App);
